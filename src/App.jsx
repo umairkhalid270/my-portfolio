06c9ea459dec5a2f12d5cbfb43932d0aad55256b
Umair Khalid <umairkhalid270@gmail.com>
2026-03-21 23:33:22 -0700

    Day1 -portfolio setup complete

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl text-blue-500 font-bold">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
