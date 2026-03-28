5d61c92aaf8710bf669660223f3de687a476db94
Umair Khalid <umairkhalid270@gmail.com>
2026-03-28 06:21:25 -0700

    js to cjs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const handler = require('./api/chat.js');
+const handler = require('./api/chat.cjs');
 app.post('/api/chat', handler);
 
 app.listen(3001, () => {
